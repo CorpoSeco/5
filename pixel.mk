b95a636d544827e0b34ad6d276558b1721bfbe6d
User <User@gmail.com>
2023-02-02 11:21:38 -0300

    add

---
+++ pixel.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/BoardConfigSoong.mk)
+$(call inherit-product, device/custom/sepolicy/common/sepolicy.mk)
+-include vendor/aosp/build/core/config.mk
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_USES_PREBUILT_VENDOR_SEPOLICY := true
+TARGET_HAS_FUSEBLK_SEPOLICY_ON_VENDOR := true
+
+PRODUCT_PACKAGES += \
+    libaptX_encoder \
+    libaptXHD_encoder
